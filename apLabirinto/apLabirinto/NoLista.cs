ffdae8c5ef22a2a57144ee316a3f385d439db001
Gabriel Scalese <cc19171@g.unicamp.br>
2020-08-18 08:17:58 -0300

    Initialized project

---
+++ apLabirinto/apLabirinto/NoLista.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace apLabirinto
+{
+    public class NoLista<Dado> where Dado : IComparable<Dado>
+    {
+        private Dado dado;
+        private NoLista<Dado> prox;
+
+        public Dado Info
+        {
+            get => dado;
+            set
+            {
+                if (value != null)
+                    dado = value;
+            }
+        }
+
+        public NoLista<Dado> Prox
+        {
+            get => prox;
+            set
+            {
+                prox = value;
+            }
+        }
+
+        public NoLista (Dado dado, NoLista<Dado> prox)
+        {
+            Info = dado;
+            Prox = prox;
+        }
+    }
+}
---
+++ apLabirinto/apLabirinto/PilhaLista.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace apLabirinto
+{
+    public class PilhaLista<Dado> where Dado : IComparable<Dado>
+    {
+        private ListaSimples<Dado> listaSimples;
+
+        public void Empilhar (Dado dado)
+        {
+            listaSimples.InsiraNoFim(dado);
+        }
+
+        public Dado Desimpilhar ()
+        {
+            Dado dado = listaSimples.GetUltimo();
+            listaSimples.RemovaDoFim();
+
+            return dado;
+        }
+
+        public PilhaLista ()
+        {
+            listaSimples = new ListaSimples<Dado>();
+        }
+    }
+}
